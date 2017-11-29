13204720d6dbda69c664c42609a8d0d8f52fc3bb
Arthur201 <arthurlaureanosilva2001@gmail.com>
2017-11-29 15:30:13 -0200

    Adicionada logica da Posicao de Xadrez

--- projeto-final/Program.cs
+++ projeto-final/Program.cs
@@ -8,21 +8,11 @@ namespace projeto_final
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c ', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.preta), new Posicao(0, 2));
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
 
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
 
             Console.ReadLine(); 
 

19e9d908ac96c4f11653d70ffcb19a13184b068b
Arthur201 <arthurlaureanosilva2001@gmail.com>
2017-12-13 10:51:31 -0200

    Controlando as pecas capturadas

--- projeto-final/Program.cs
+++ projeto-final/Program.cs
@@ -18,11 +18,7 @@ namespace projeto_final
                     {
 
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
